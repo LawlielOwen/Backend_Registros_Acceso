044e5163a630b4bed3c47548e14419dfdfb6e661
OsvaldoGM(El rogon) <Karypekito@gmail.com>
2026-04-03 11:28:06 -0600

    db

--- config/db.ts
+++ config/db.ts
@@ -3,13 +3,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 4, // <-- Bajamos el límite para que Clever Cloud no lo bloquee
+  ssl: {
+    rejectUnauthorized: false // <-- Permite la conexión segura entre Render y Clever Cloud
+  }
 });
 
 export default pool;
