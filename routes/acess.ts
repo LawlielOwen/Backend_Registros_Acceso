21eb3618bfbfad8d6ae8f0863a1e27a3e70df7c6
OsvaldoGM(El rogon) <Karypekito@gmail.com>
2026-04-09 15:19:05 -0600

    x

--- routes/acess.ts
+++ routes/acess.ts
@@ -38,27 +38,23 @@ router.get("/", async (_req: Request, res: Response) => {
 router.get("/count", async (req: Request, res: Response) => {
   try {
     const { fecha, id_punto, exitoso } = req.query;
-
     let query = `
       SELECT 
         COUNT(CASE WHEN tipo_acceso = 'Entrada' THEN 1 END) AS entradas,
         COUNT(CASE WHEN tipo_acceso = 'Salida' THEN 1 END) AS salidas
-      FROM vista_registros_acceso
-      WHERE DATE(fecha_acceso) = DATE(DATE_SUB(NOW(), INTERVAL 8 HOUR))`; 
-      
+      FROM tbl_registros_acceso
+      WHERE 1=1
+    `;
     const params: any[] = [];
-    
     if (id_punto) {
       query += " AND id_punto = ?";
       params.push(Number(id_punto));
     }
-    
     if (fecha) {
       query += " AND DATE(fecha_acceso) = ?";
       params.push(fecha);
     } else {
-      // CORRECCIÓN 2: Y también ajustarlo en el bloque else
-      query += " AND DATE(fecha_acceso) = DATE(DATE_SUB(NOW(), INTERVAL 8 HOUR))";
+      query += " AND DATE(fecha_acceso) = CURDATE()";
     }
 
     if (exitoso !== undefined) {
